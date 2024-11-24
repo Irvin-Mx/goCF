ed6e297e2dc7ede8f1ebcc37c3933ab3512bf12c
Irvin Martinez <irvin0329@outlook.com>
2024-11-24 09:26:54 -0700

    SecuencialValues

--- main.go
+++ main.go
@@ -2,27 +2,44 @@ package main // name of our package
 
 import "fmt" //Imports the formatting package from Go's standard library, which provides functions for formatted I/O
 
+//Secuencial constants example
+// const a int = 0
+// const b int = 1
+// const c int = 3
+// const d int = 4
+// const e int = 5
+// const f int = 6
+// const g int = 7
+//*Refactored below
+// const (
+// 	a int = 0
+// 	b int = 1
+// 	c int = 3
+// 	d int = 4
+// 	e int = 5
+// 	f int = 6
+// 	g int = 7
+// )
+
+//*Iota example
+ const (
+ 	a int = iota * 10 // Will start at 0, if needed can make other operations like +,-,*,/
+ 	b
+ 	c
+ 	d
+ 	e
+ 	f
+ 	g
+ )
+
+
 //The main function is the entry point of the program. It's where execution begins
 func main() {
-	var isHigh string // empty string "" initial value
-	var pressure int // initial value 0
-	var frequency = 32.32
-	otherVariable := true
-
-	var temp int = 99
-
-	var maxTemp int
-	maxTemp = 90
-	if temp > maxTemp {
-		fmt.Println("The temperature is too high")
-	}
-	for i := 0; i < 10; i++ {
-		temp ++
-	}
-	fmt.Println(isHigh)
-	fmt.Println(pressure)
-	fmt.Println(temp)
-	fmt.Println(otherVariable)
-	fmt.Println(frequency)
-	fmt.Println("Is high:",isHigh,"Pressure:",pressure,"Temp:",temp,"Other Variable:",otherVariable,"Frequency:",frequency)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
 }
