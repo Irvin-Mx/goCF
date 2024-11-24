70f07802ca1248b23fee5663dbe62b3af28cbb64
Irvin Martinez <irvin0329@outlook.com>
2024-11-23 20:31:22 -0700

    Declaring variables in Go

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
-package main
+package main // name of our package
 
-import "fmt"
+import "fmt" //Imports the formatting package from Go's standard library, which provides functions for formatted I/O
 
+//The main function is the entry point of the program. It's where execution begins
 func main() {
-	fmt.Print("Learning the awsome Go!")
+	var isHigh string // empty string "" initial value
+	var pressure int // initial value 0
+	var frequency = 32.32
+	otherVariable := true
+
+	var temp int = 99
+
+	var maxTemp int
+	maxTemp = 90
+	if temp > maxTemp {
+		fmt.Println("The temperature is too high")
+	}
+	for i := 0; i < 10; i++ {
+		temp ++
+	}
+	fmt.Println(isHigh)
+	fmt.Println(pressure)
+	fmt.Println(temp)
+	fmt.Println(otherVariable)
+	fmt.Println(frequency)
+	fmt.Println("Is high:",isHigh,"Pressure:",pressure,"Temp:",temp,"Other Variable:",otherVariable,"Frequency:",frequency)
 }
